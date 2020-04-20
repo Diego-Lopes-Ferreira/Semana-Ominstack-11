4a3ceafd0c16694de5f89f01ca0931d3b94b057f
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-04-19 21:03:35 -0300

    Test update

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,52 +11,19 @@ const routes = express.Router();
 
 //routes
 //incidents
-routes.get('/incidents', celebrate({
-  [Segments.QUERY]: Joi.object().keys({
-    page: Joi.number()
-  })
-}), incidentController.index);
-routes.post('/incidents', celebrate({
-  [Segments.HEADERS]: Joi.object({
-    authorization: Joi.string().required()
-  }).unknown(),
-  [Segments.BODY]: Joi.object().keys({
-    title: Joi.string().required(),
-    description: Joi.string().required(),
-    value: Joi.number().required(),
-  })
-}), incidentController.createIncident);
-routes.delete('/incidents/:id', celebrate({
-  [Segments.PARAMS]: Joi.object().keys({
-    id: Joi.number().required()
-  })
-}), incidentController.deleteIncident);
+routes.get('/incidents', incidentController.index);
+routes.post('/incidents', incidentController.createIncident);
+routes.delete('/incidents/:id', incidentController.deleteIncident);
 
 //ongs
 routes.get('/ongs', ongController.index);
-routes.post('/ongs',celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    whatsapp: Joi.string().required().min(10).max(11),
-    cidade: Joi.string().required(),
-    uf: Joi.string().required().length(2)
-  })
-}), ongController.createOng);
+routes.post('/ongs', ongController.createOng);
 
 //profile
-routes.get('/profile', celebrate({
-  [Segments.HEADERS]: Joi.object({
-    authorization: Joi.string().required()
-  }).unknown()
-}), profileController.index);
+routes.get('/profile', profileController.index);
 
 //sessions
-routes.post('/sessions', celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    id: Joi.number().required()
-  })
-}), sessionsController.login);
+routes.post('/sessions', sessionsController.login);
 
 //index (just a test)
 routes.get('/', (request, response) => {
